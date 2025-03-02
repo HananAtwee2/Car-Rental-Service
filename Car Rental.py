5f326627bef36ee7e16c21f83e6a248828333cdc
Your Name <atwihanan20@gmail.com>
2025-03-02 22:58:52 +0200

    setter and getter of rental price is added

--- Car Rental.py
+++ Car Rental.py
@@ -9,8 +9,20 @@ class Vehicle:
         self.brand = brand
         self.model = model
         self.year = year
-        self.__rental_price_per_day = rental_price_per_day
+        self.__rental_price_per_day = rental_price_per_day #private
 
     #display function
     def display_info(self):
         print()
+
+    def calculate_rental_cost(self,days):
+        return self.rental_price_per_day * days
+    #getter
+    def get_rental_price(self):
+        return self.__rental_price_per_day   
+    #setter
+    def set_rental_price(self,new_price):
+        if new_price > 0:
+            self.__rental_price_per_day = new_price
+        else:
+            print("Price must be positive.")
