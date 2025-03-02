824ebb858c7cb0a01dd4e894d4bfedb0c4153e52
Your Name <atwihanan20@gmail.com>
2025-03-02 21:41:14 +0200

    Display function not completed

--- Car Rental.py
+++ Car Rental.py
@@ -4,3 +4,13 @@ class Vehicle:
     year=0
     rental_price_per_day=0
     
+    #Constructor
+    def _init_(self, brand,model, year, rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.__rental_price_per_day = rental_price_per_day
+
+    #display function
+    def display_info(self):
+        print()
